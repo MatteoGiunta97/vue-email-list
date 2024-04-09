589fea721290910414f929fcad71ae0e9b3197f9
MatteoGiunta97 <matteogiunta@outlook.it>
2024-04-09 15:52:44 +0200

    feat: add ul/li

--- js/script.js
+++ js/script.js
@@ -11,19 +11,20 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 const { createApp } = Vue;
 
 createApp({
-data() {
-    return {
-        emailList: null
-    };
-}
-,
-methods: {
-},
-mounted() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-            const serverData = response.data;
-            this.emailList = serverData.response;
-        });
-}
+    data() {
+        return {
+            emailList: null
+        };
+    }
+    ,
+    methods: {
+    }
+    ,
+    mounted() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const serverData = response.data;
+                this.emailList = serverData.response;
+            });
+    }
 }).mount('#app')
